49b51389edeb83a471b08c5cdf394436a79c8551
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-03 13:56:28 +0500

    contact api written

---
+++ React/src/contact.js
@@ -0,0 +1,74 @@
+import localforage from "localforage";
+import {matchSorter} from "match-sorter";
+import sortBy from "sort-by";
+
+export async function getContacts(query){
+    await fakenetwork(`getContacts:${query}`);
+    let contacts = await localforage.getItem("contacts");
+    if(!contacts){
+        contacts = [];
+    }
+    if(query){
+        contacts = matchSorter(contacts,query,{keys:["first","last"]});
+    }
+    return contacts.sort(sortBy("last","createdAt"));
+}
+
+export async function createContact(){
+    await fakenetwork();
+    let id = Math.random().toString(36).substring(2,9);
+    let contact = {id, createdAt:Date.now()};
+    let contacts = await getContacts();
+    contacts.unshift(contact);
+    await set(contacts);
+    return contact;
+}
+
+export async function getContact(id){
+    await fakenetwork(`contact:${id}`);
+    let contacts = await localforage.getItem("contacts");
+    let contact = contacts.find(contact => contact.id === id);
+    return contact ?? null;
+}
+
+export async function updateContact(id, updates){
+    await fakenetwork();
+    let contacts = await localforage.getItem("contacts");
+    let contact = contacts.find(contact => contact.id === id);
+    if(!contact){
+        throw new Error("No contact found for",id);
+    }
+    Object.assign(contact,updates);
+    await set(contacts);
+    return contact;
+}
+
+export async function deleteContact(id){
+
+    let contacts = await localforage.getItem("contacts");
+    let index = contacts.findIndex(contact => contact.id === id);
+    if (index > -1){
+        contacts.splice(index,1);
+        await set (contacts);
+        return true;
+    }
+    return false;
+
+}
+
+function set(contacts){
+    return localforage.setItem("contacts", contacts);
+}
+let fakeCache = {};
+async function fakenetwork(key){
+    if(!key){
+        fakeCache = {};
+    }
+    if(fakeCache[key]){
+        return;
+    }
+    fakeCache[key] = true;
+    return new Promise(res => {
+        setTimeout(res,Math.random()*800);
+    });
+}
