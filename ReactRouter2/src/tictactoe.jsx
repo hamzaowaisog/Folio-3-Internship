9ca656dc2e2c1906a41edc5d33ef8575ae5ee550
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-06 06:23:28 +0500

    feat: Add winner status and update next player status in Tic Tac Toe game

--- ReactRouter2/src/tictactoe.jsx
+++ ReactRouter2/src/tictactoe.jsx
@@ -5,7 +5,7 @@ export default function Tictactoe() {
     const [xIsNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   function handleClick(i) {
-    if(squares[i]){
+    if(squares[i] || calculateWinner(squares)){
         return;
     }
     const nextSquares = squares.slice();
@@ -18,6 +18,17 @@ export default function Tictactoe() {
     setSquares(nextSquares);
     setXisNext(!xIsNext);
   }
+
+  const winner = calculateWinner(squares);
+  let status ;
+  if(winner){
+        status = "Winner: " + winner;
+        // alert(`Winner is ${winner}`);
+  }
+    else{
+        status = "Next Player: " + (xIsNext ? "X" : "O");
+    }
+
   return (
     <>
       <div>
@@ -27,6 +38,7 @@ export default function Tictactoe() {
       </div>
       <div className="container">
         <div>
+            <div className="status">{status}</div>
           <div className="board-row">
             <Square
               id="s1"
@@ -91,6 +103,28 @@ function Square({ id, value, onSquareClick }) {
     </button>
   );
 }
+
+function calculateWinner(squares){
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let i=0; i<lines.length; i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+
+}
+
 Square.PropTypes = {
   id: PropTypes.string,
 };
