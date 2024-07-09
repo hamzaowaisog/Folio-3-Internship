c4b419575e225cbde160584e5f9cbac2e594b78e
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-09 17:56:23 +0500

    refactor: Update SheyPizza header to use CSS classes for styling

--- SheyPizza/src/Components/MainComponent.jsx
+++ SheyPizza/src/Components/MainComponent.jsx
@@ -2,19 +2,15 @@ import { Layout } from "antd";
 import HeaderClass from "./headerClass";
 const { Header, Content } = Layout;
 import PizzaContent from "./Content";
+import "../CSS/MainComponent.css";
 
 export default function MainComponent(){
     return(
         <Layout>
-            <Header style={{
-            backgroundColor: "#fff",
-            display: "flex",
-            justifyContent: "space-between",
-            boxShadow: "0px 1px 30px rgba(0,0,0,0.75)",
-          }}>
+            <Header className="header">
                 <HeaderClass/>
             </Header>
-            <Content style={{ padding: "0 50px", marginTop: "20px" }}>
+            <Content className="content">
                 <PizzaContent/>
             </Content>
         </Layout>
