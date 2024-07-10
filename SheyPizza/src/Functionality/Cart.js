5fd7bccf931c6353d45a65e6f37996cd24d4dbee
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-10 17:56:26 +0500

    refactor: Update CartFunction in Cart.js to include price in addToCart function

--- SheyPizza/src/Functionality/Cart.js
+++ SheyPizza/src/Functionality/Cart.js
@@ -1,25 +1,28 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const CartFunction = () => {
-    const [cart , setCart] = useState([]);
-    
-    const addToCart = (pizza , variant , quantity) =>{
-        setCart((prevCart) => {
-            const existingItemIndex = prevCart.findIndex(
-                (item) => item.pizza.name === pizza.name && item.variant === variant
-            );
-            if (existingItemIndex !== -1) {
-                const updatedCart = [...prevCart];
-                updatedCart[existingItemIndex].quantity += quantity;
-                return updatedCart;
-            }
-            return [...prevCart, {pizza,variant , quantity}];
-        });
-    };
-    return{
-        cart,
-        setCart,
-        addToCart
-    };
-}
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (pizza, variant, quantity, price) => {
+    setCart((prevCart) => {
+      const existingItemIndex = prevCart.findIndex(
+        (item) => item.pizza.name === pizza.name && item.variant === variant
+      );
+      if (existingItemIndex !== -1) {
+        return prevCart.map((item, index) =>
+          index === existingItemIndex
+            ? { ...item, quantity: item.quantity + quantity }
+            : item
+        );
+      }
+      return [...prevCart, { pizza, variant, quantity, price }];
+    });
+  };
+
+  return {
+    cart,
+    addToCart,
+  };
+};
+
 export default CartFunction;
