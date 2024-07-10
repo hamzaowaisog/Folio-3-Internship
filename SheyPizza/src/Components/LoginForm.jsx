eee6f5f94b5f34d3b371bca539c4e560a5502cc8
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-10 12:34:43 +0500

    refactor: Update SheyPizza login form with Ant Design components

--- SheyPizza/src/Components/LoginForm.jsx
+++ SheyPizza/src/Components/LoginForm.jsx
@@ -1,13 +1,82 @@
 
-export default function LoginForm(){
-    return(
-        <div>
-            <h1>Login</h1>
-            <form>
-                <input type="text" placeholder="Username"/>
-                <input type="password" placeholder="Password"/>
-                <button>Login</button>
-            </form>
-        </div>
-    )
+import { Button, Checkbox, Form, Input } from "antd";
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
+
+export default function LoginForm() {
+  return (
+   <div>
+    <h1>LOGIN</h1>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="Email"
+        rules={[
+          {
+            required: true,
+            message: "Please input your Email!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" danger htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>
+  );
 }
