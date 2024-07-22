1e30337d52a3004e86972c76710a673f3d63c531
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-22 16:49:45 +0500

    chore: Add Redux store and authentication slice

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import { useSelector } from "react-redux";
 
 function App() {
+
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  console.log(isAuthenticated);
   return (
     <div className="App">
-      <Auth />
-      {/* <Layout /> */}
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <Layout />}
     </div>
   );
 }
