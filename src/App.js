f842a31ea0a8cecaa1cadea0352829988f5886e2
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-23 18:19:17 +0500

    chore: Add UI slice to Redux store for managing notification state

--- src/App.js
+++ src/App.js
@@ -2,28 +2,65 @@ import React, { useEffect } from "react";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import Notifiction from "./components/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let firstRender = true;
 
 function App() {
   const cart = useSelector((state) => state.cart);
-
+  const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   console.log(isAuthenticated);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
+    if (firstRender) {
+      firstRender = false;
+      return;
+    }
+
     const sendRequest = async () => {
-      const res = await fetch("https://redux-http-6581f-default-rtdb.firebaseio.com/cartItems.json",{
-        method : 'PUT',
-        body: JSON.stringify(cart),
-      });
+      dispatch(
+        uiActions.showNotification({
+          message: "Sending cart data",
+          type: "warning",
+          open: true,
+        })
+      );
+      const res = await fetch(
+        "https://redux-http-6581f-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
       const data = await res.json();
+      dispatch(
+        uiActions.showNotification({
+          message: "Sent cart data successfully",
+          type: "success",
+          open: true,
+        })
+      );
     };
-    sendRequest();
-    
+    sendRequest().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          message: "Sending cart data failed",
+          type: "error",
+          open: true,
+        })
+      );
+    });
   }, [cart]);
 
   return (
     <div className="App">
+      {notification && (
+        <Notifiction type={notification.type} message={notification.message} />
+      )}
       {!isAuthenticated && <Auth />}
       {isAuthenticated && <Layout />}
     </div>
