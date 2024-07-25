27ca53abe15939a221c8aa97f6c6a37ef004243d
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-25 17:21:50 +0500

    chore: Refactor useFetchData hook to handle loading state in PizzaContent component

--- SheyPizza/src/API/useFetch.js
+++ SheyPizza/src/API/useFetch.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { getData } from "./api";
 
-export const fetchPizzaData = createAsyncThunk('pizza/fetchData', async(endpoint,{rejectWithValue})=>{
+export const fetchPizzaData = createAsyncThunk('pizza/fetchData', async(endpoint)=>{
   return getData(endpoint).then(response => response.data);
 });
