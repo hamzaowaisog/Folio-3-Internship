fc6ad46510b9b68e2968df759292acf31c38a825
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-07-29 15:57:39 +0500

    Refactor RoleBaseRoute component to use useRoleBasedAccess hook for role-based access control

---
+++ SheyPizza/src/Components/AddView.jsx
@@ -0,0 +1,9 @@
+
+
+export default function AddView(){
+    return(
+        <div>
+            <h1> AddView </h1>
+        </div>
+    )
+}
--- SheyPizza/src/Private/Routes/RoleBaseRoute.jsx
+++ SheyPizza/src/Private/Routes/RoleBaseRoute.jsx
@@ -1,23 +1,18 @@
-import { useSelector } from "react-redux"
-import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Navigate } from "react-router-dom"; // Import the Navigate component
+import useRoleBasedAccess from "../../Functionality/useRoleBasedAcces";
 
-const RoleBaseRoute = ({ element:Element , requiredRole, ...rest}) =>{
-RoleBaseRoute.propTypes = {
-  element: PropTypes.elementType.isRequired,
-  requiredRole: PropTypes.string.isRequired,
+const RoleBaseRoute = ({ element: Element, requiredRole, ...rest }) => {
+  RoleBaseRoute.propTypes = {
+    element: PropTypes.elementType.isRequired,
+    requiredRole: PropTypes.string.isRequired,
+  };
+  const redirect = useRoleBasedAccess(requiredRole);
+
+  if (redirect) {
+    return <Navigate to={redirect}/>;
+  }
+  console.log("Rendering element:", Element);
+  return <Element {...rest} />;
 };
-    const {isAdmin,isAuthenticated , data} = useSelector(state => state.auth);
-
-    if(!isAuthenticated){
-        console.log(isAuthenticated);
-        return <Navigate to='/login'/>;
-    }
-    if(!data && !isAdmin && requiredRole !== 'admin'){
-        return <Navigate to='/'/>
-    }
-    console.log('Rendering element:', Element);
-    return <Element {...rest}/>;
-
-}
 export default RoleBaseRoute;
