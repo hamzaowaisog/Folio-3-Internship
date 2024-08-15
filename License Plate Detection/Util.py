bd0ad504fe53ab0f6f9b2f7eee5cb484a9288112
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-08-15 12:53:06 +0500

    refactor: Update license plate detection algorithm and tracking

--- License Plate Detection/Util.py
+++ License Plate Detection/Util.py
@@ -100,18 +100,18 @@ def read_license_plate(license_plate_crop):
 
 
 def get_car(license_plate, vehicle_track_ids):    
-    # x1, y1, x2, y2, score, class_id = license_plate
+    x1, y1, x2, y2, score, class_id = license_plate
 
-    # foundIt = False
-    # for j in range(len(vehicle_track_ids)):
-    #     xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
+    foundIt = False
+    for j in range(len(vehicle_track_ids)):
+        xcar1, ycar1, xcar2, ycar2, car_id = vehicle_track_ids[j]
 
-    #     if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
-    #         car_indx = j
-    #         foundIt = True
-    #         break
+        if x1 > xcar1 and y1 > ycar1 and x2 < xcar2 and y2 < ycar2:
+            car_indx = j
+            foundIt = True
+            break
 
-    # if foundIt:
-    #     return vehicle_track_ids[car_indx]
+    if foundIt:
+        return vehicle_track_ids[car_indx]
 
-    return 0, 0, 0, 0, 0
+    return -1,-1,-1,-1,-1
