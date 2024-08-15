4532546ea76a239c47e83ccaa1db4d93d06316dc
Mir Hamza <122245138+hamzaowaisog@users.noreply.github.com>
2024-08-15 12:55:51 +0500

    update main.py by adding check for car id

--- License Plate Detection/Main.py
+++ License Plate Detection/Main.py
@@ -44,26 +44,27 @@ while ret:
                 #assign license plate to car
                 xcar1,ycar1,xcar2,ycar2,car_id = get_car(license_plate,track_ids)
                 
-                #crop license plate
-                license_plate_crop = frame[int(y1):int(y2),int(x1):int(x2), :]
+                if car_id != -1:
+                    #crop license plate
+                    license_plate_crop = frame[int(y1):int(y2),int(x1):int(x2), :]
                 
-                #process license plate
+                    #process license plate
                 
-                license_plate_crop_grey = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
-                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_grey, 64, 255, cv2.THRESH_BINARY_INV)
+                    license_plate_crop_grey = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+                    _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_grey, 64, 255, cv2.THRESH_BINARY_INV)
                 
-                # cv2.imshow('original crop' , license_plate_crop)
-                # cv2.imshow('threshold' , license_plate_crop_thresh)
+                    # cv2.imshow('original crop' , license_plate_crop)
+                    # cv2.imshow('threshold' , license_plate_crop_thresh)
                 
-                # cv2.waitKey(0)
+                    # cv2.waitKey(0)
                 
-                #reading license plate
+                    #reading license plate
                 
-                license_plate_text , license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                    license_plate_text , license_plate_text_score = read_license_plate(license_plate_crop_thresh)
                 
-                if license_plate_text is not None:
-                    results[frame_num][car_id] = {'car':{'bbox':[xcar1,ycar1,xcar2,ycar2]} , 
-                                                  'license_plate':{'bbox': [x1,y1,x2,y2],
+                    if license_plate_text is not None:
+                        results[frame_num][car_id] = {'car':{'bbox':[xcar1,ycar1,xcar2,ycar2]} , 
+                                                    'license_plate':{'bbox': [x1,y1,x2,y2],
                                                                    'text': license_plate_text, 
                                                                    'bbox_score' : score, 
                                                                    'text_score': license_plate_text_score}} 
